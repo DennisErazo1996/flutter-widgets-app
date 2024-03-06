67a962217aa68111aab33f523f8b73c9d7f004d6
DennisErazo1996 <dennis.erazo96@gmail.com>
2024-03-06 12:17:58 -0600

    Rutas basicas configuradas

--- lib/config/presentation/screens/home/home_screen.dart
+++ lib/config/presentation/screens/home/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/config/presentation/screens/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,6 +11,61 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Flutter + Material 3'),
       ),
+      body: _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //const menuItems = appMenuItems;
+
+    return ListView.builder(
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, index) {
+        final menuItems = appMenuItems[index];
+
+        return _CustomListTile(menuItems: menuItems);
+      },
+    );
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  const _CustomListTile({
+    required this.menuItems,
+  });
+
+  final MenuItem menuItems;
+
+  @override
+  Widget build(BuildContext context) {
+    final color = Theme.of(context).colorScheme;
+
+    return ListTile(
+      leading: Icon(
+        menuItems.icon,
+        color: color.primary,
+      ),
+      trailing: Icon(
+        Icons.arrow_forward_ios_rounded,
+        color: color.primary,
+      ),
+      title: Text(menuItems.title),
+      subtitle: Text(menuItems.subTitle),
+      titleTextStyle:
+          TextStyle(color: color.primary, fontWeight: FontWeight.bold),
+      onTap: () {
+        /*Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) =>  const ButtonsScreen(),
+          ),
+        );*/
+
+        Navigator.pushNamed(context, menuItems.link);
+      },
+      hoverColor: Colors.green[70],
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/presentation/screens/buttons/buttons_screen.dart';
+import 'package:widgets_app/config/presentation/screens/cards/cards_screen.dart';
 import 'package:widgets_app/config/presentation/screens/home/home_screen.dart';
 import 'package:widgets_app/config/theme/app_theme.dart';
 
@@ -12,9 +14,13 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: AppTheme(selectedColor: 1).getTheme(),
+      theme: AppTheme(selectedColor: 2).getTheme(),
       debugShowCheckedModeBanner: false,
-      home: const HomeScreen()
+      home: const HomeScreen(),
+      routes: {
+        '/buttons': (context) => const ButtonsScreen(),
+        '/cards': (context) => const CardsScreen(),
+      },
     );
   }
 }
