0fab51a8ee96d7b455e0e24f81dad5eeb7bf00b9
DennisErazo1996 <dennis.erazo96@gmail.com>
2024-04-26 10:28:02 -0600

    go_router

--- lib/config/presentation/screens/home/home_screen.dart
+++ lib/config/presentation/screens/home/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
-import 'package:widgets_app/config/presentation/screens/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -62,8 +62,8 @@ class _CustomListTile extends StatelessWidget {
             builder: (context) =>  const ButtonsScreen(),
           ),
         );*/
-
-        Navigator.pushNamed(context, menuItems.link);
+        context.push(menuItems.link);
+        //Navigator.pushNamed(context, menuItems.link);
       },
       hoverColor: Colors.green[70],
     );
